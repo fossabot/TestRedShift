398cd3f341b5d43bf49b99a8b8077f6e12772723
ignatandrei <ignatandrei@yahoo.com>
2019-02-05 00:04:02 +0200

    error in =

--- AntWeb1/src/app/adv-search/ChecklistDatabase.ts
+++ AntWeb1/src/app/adv-search/ChecklistDatabase.ts
@@ -1,8 +1,8 @@
-import { AdvfindService } from '../advfind.service';
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { TodoItemNode } from './TodoItemNode';
-import { TodoItemArray } from './TodoItemArray';
+import { AdvfindService } from "../advfind.service";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { TodoItemNode } from "./TodoItemNode";
+import { TodoItemArray } from "./TodoItemArray";
 /**
  * The Json object for to-do list data.
  */
@@ -14,16 +14,13 @@ export class ChecklistDatabase {
     //this.initialize();
   }
   //TODO: user https://angular.io/guide/dynamic-component-loader
-  initializeTopics(){
-    this.adv.getTopics().subscribe(it=>{
+  initializeTopics() {
+    this.adv.getTopics().subscribe(it => {
       console.log(`number received : ${it.length}`);
       const data = TodoItemArray.create();
 
-      it.forEach((top) => {
-        
-        
-          data.push(TodoItemNode.FromTopic(top));
-        
+      it.forEach(top => {
+        data.push(TodoItemNode.FromTopic(top));
       });
       this.dataChange.next(data);
     });
@@ -35,32 +32,29 @@ export class ChecklistDatabase {
       var newArr = it.slice();
       console.log(`number received : ${newArr.length}`);
       const data = TodoItemArray.create();
-      it.forEach((spec) => {
+
+      it.forEach(spec => {
         if (spec.idParent == null) {
           data.push(TodoItemNode.FromSpec(spec));
           const index = newArr.indexOf(spec, 0);
-          newArr = newArr.splice(index, 1);
+          newArr.splice(index, 1);
         }
       });
       console.log(`after removing null : ${newArr.length}`);
       while (newArr.length > 0) {
         console.log(`number elements : ${newArr.length}`);
-        it.forEach((spec) => {
-          if(spec.idParent == null)
-            return;
-
-          var item = data.FindParent(spec.idParent);
-          if (item != null) {
-            var id = spec.id;
-            if(item.children.filter(c=>c.id ==id).length == 0){
-
-              var newNode = TodoItemNode.FromSpec(spec);
-              item.children.push(newNode);
-              const index = newArr.findIndex(it=>it.id == id);
-              newArr = newArr.splice(index, 1);
-            
-            }            
-            
+        it.forEach(spec => {
+          if (spec.idParent != null) {
+            var item = data.FindParent(spec.idParent);
+            if (item != null) {
+              var id = spec.id;
+              if (item.children.filter(c => c.id == id).length == 0) {
+                var newNode = TodoItemNode.FromSpec(spec);
+                item.children.push(newNode);
+                const index = newArr.findIndex(it => it.id == id);
+                newArr.splice(index, 1);
+              }
+            }
           }
         });
       }
