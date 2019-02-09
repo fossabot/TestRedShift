c8d1f7d0f5d2b40adc1ed0e5650cd7646c0a2f26
ignatandrei <ignatandrei@yahoo.com>
2019-02-09 16:08:13 +0200

    added controller

---
+++ Ant/Ant/Controllers/UtilsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ant.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    //[ApiController]
+    public class UtilsController : ControllerBase
+    {
+        [HttpGet]
+        public string Ping()
+        {
+            return DateTime.UtcNow.ToString("s", System.Globalization.CultureInfo.InvariantCulture);
+        }
+        [HttpGet]
+        public FileVersionInfo[] GetVersions([FromServices] IHostingEnvironment hosting)
+        {
+            var dirPath = hosting.ContentRootPath;
+            var ret = new List<FileVersionInfo>();
+            var files = Directory.EnumerateFiles(dirPath, "*.dll", SearchOption.AllDirectories)
+                .Union(Directory.EnumerateFiles(dirPath, "*.exe", SearchOption.AllDirectories));
+
+            foreach (string item in files)
+            {
+                try
+                {
+                    var info = FileVersionInfo.GetVersionInfo(item);
+                    ret.Add(info);
+
+                }
+                catch (Exception)
+                {
+                    //TODO: log
+                }
+            }
+            return ret.ToArray();
+        }
+    }
+}
