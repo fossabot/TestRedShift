0f3b14f147dee64d6714d821d12e83df0e1836a3
Andrei Ignat <ignatandrei@yahoo.com>
2019-09-09 21:53:45 +0300

    show

--- AntWeb1/src/app/adv-search-new/adv-search-new.component.ts
+++ AntWeb1/src/app/adv-search-new/adv-search-new.component.ts
@@ -3,17 +3,28 @@ import { AdvSearchNewService } from '../adv-search-new.service';
 import { NewCountry } from '../NewCountry';
 import { map, filter, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { fromEvent } from 'rxjs';
+import { TouchSequence } from 'selenium-webdriver';
+class FoundCountry{
+  constructor(){
+    this.ids=[];
+  }
+  name: string;
+  ids: Array<number>;
 
+}
 @Component({
   selector: 'app-adv-search-new',
   templateUrl: './adv-search-new.component.html',
   styleUrls: ['./adv-search-new.component.css']
 })
 export class AdvSearchNewComponent implements OnInit {
+  FoundCountries: Map< string, FoundCountry>;
 
   nrResultsCountries: number;
   //folders: NewCountry[];
-  constructor(private  adv: AdvSearchNewService) { }
+  constructor(private  adv: AdvSearchNewService) {
+    this.FoundCountries=new Map<string,FoundCountry>();
+   }
 
   ngOnInit() {
   //    this.adv.GetCountry(0).subscribe(it=>this.folders=it);
@@ -24,12 +35,27 @@ export class AdvSearchNewComponent implements OnInit {
     debounceTime(3*1000),
     distinctUntilChanged(),  
     switchMap(it=>this.adv.SearchCountryFromKingdoms(it))
+
   );
   
   typeahead.subscribe(data => {
    // Handle the data from the API
    console.log('changed' + data.length);
    this.nrResultsCountries = data.length;
+   
+   this.FoundCountries.clear();
+   data.map(it=>{
+     var name=it.name.trim();
+      if(!this.FoundCountries.has(name)){
+        var f=new FoundCountry();
+        f.name = name;
+        this.FoundCountries.set(name,f);
+        
+      }
+      this.FoundCountries.get(name).ids.push(it.id);
+      
+   });
+   console.log('nr:' + this.FoundCountries.size);
   });
 
   }
