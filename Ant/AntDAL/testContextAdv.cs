ffd38d4d1af85e201414ef166c7423b3c7d69999
Andrei Ignat <ignatandrei@yahoo.com>
2019-09-09 06:42:27 +0300

    id

--- Ant/AntDAL/testContextAdv.cs
+++ Ant/AntDAL/testContextAdv.cs
@@ -219,7 +219,7 @@ group by c.idHDCountry, c.CountryName").ToArrayAsync();
         public async ValueTask<GenericData[]> SearchCountryFromKingdoms(string name)
         {
             var data = await this.Parent.FromSql(
-                $@"select idHDCountry, CountryName as Name 
+                $@"select idHDCountry as Id, CountryName as Name 
                 ,0 as orig, 0 as IDhd
                 from CountryFromKingdoms
                 where CountryName like '%{name}%'").ToArrayAsync();
